a4d96205b00e3046d33cba2fc7bccce730e8f322
thangquoc <lequocthang307@gmail.com>
2024-11-16 15:01:21 +0700

    asfdg

---
+++ powershell_tool/dom-checklist.ps1
@@ -0,0 +1,36 @@
+﻿$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $false
+$filePath = "E:\power_shell_tutor\Dom.xlsx"
+$sheetNameToDelete = "Sheet2"
+
+$workbook = $excel.Workbooks.Open($filePath)
+$sheet = $workbook.Sheets.Item("Sheet1")
+
+$cell = $sheet.Range("I1")
+$cell.Value2 = "3"
+
+$excel.Calculate()
+
+$usedRange = $sheet.UsedRange
+foreach ($cell in $usedRange) {
+    if ($cell.HasFormula) {
+        $cell.Value2 = $cell.Value2
+    }
+}
+
+foreach ($sheet in $workbook.Sheets) {
+    if ($sheet.Name -eq $sheetNameToDelete) {
+        $sheet.Delete()
+        break
+    }
+}
+
+$workbook.Save()
+$workbook.Close()
+$excel.Quit()
+
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($sheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
+[GC]::Collect()
+[GC]::WaitForPendingFinalizers()
