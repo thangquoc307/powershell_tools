b3f18bcc61ac577f86d4d8aa92eb07d189620571
thangquoc <lequocthang307@gmail.com>
2024-11-18 06:23:43 +0700

    afdg

---
+++ powershell_tool/change-modify-date.ps1
@@ -0,0 +1,15 @@
+﻿function changeDate {
+    param (
+        [String] $filePath,
+        [String] $date
+    )
+    process {
+        $changingDate = Get-Date $date
+        $(Get-Item $filePath).CreationTime = $changingDate
+        $(Get-Item $filePath).LastWriteTime = $changingDate
+
+        Write-Output "Updated CreationTime and LastWriteTime for $filePath"
+    }
+}
+
+changeDate -filePath "E:\power_shell_tutor\CheckListAnalyze.xlsx" -date "2000-01-01 08:00:00"
