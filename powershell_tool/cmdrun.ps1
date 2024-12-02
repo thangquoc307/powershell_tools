e4022ea0dab1a335ec2766745b746352f4195284
thangquoc <lequocthang307@gmail.com>
2024-12-02 20:34:50 +0700

    asdf

---
+++ powershell_tool/cmdrun.ps1
@@ -0,0 +1,23 @@
+﻿function Run-CmdWithCommands {
+    param (
+        [string[]]$Commands
+    )
+    
+    # Tạo file batch tạm để chạy các lệnh
+    $tempBatchFile = [System.IO.Path]::GetTempFileName() -replace '\.tmp$', '.bat'
+
+    # Ghi các lệnh vào file batch
+    $Commands | Out-File -FilePath $tempBatchFile -Encoding ASCII
+
+    # Chạy file batch bằng cmd.exe
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$tempBatchFile`"" -NoNewWindow -Wait
+
+    # Xóa file batch tạm
+    Remove-Item -Path $tempBatchFile -Force
+}
+
+# Sử dụng hàm
+Run-CmdWithCommands -Commands @(
+    "echo Xin!",
+    "echo alo."
+)
---
+++ powershell_tool/export_winmerge.ps1
@@ -0,0 +1,9 @@
+﻿$winmergepath = "C:\Program Files\WinMerge\WinMergeU.exe"
+$file1 = "E:\power_shell_tutor\testwinmerge\New folder\test_readline_ex_cmt.pc"
+$file2 = "E:\power_shell_tutor\testwinmerge\New folder\test_readline_ex_cmtcopy.pc"
+
+$reportPath = "E:\power_shell_tutor\testwinmerge\ComparisonReport.html"
+
+
+
+& $winmergepath $file1 $file2 /r /e /u /wl /wr /x /o $reportPath
